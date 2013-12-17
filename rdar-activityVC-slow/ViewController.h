9857822a3628719bcb1b660223ab3e1ca801a745
StuFF mc <mc@stuffmc.com>
2013-12-17 18:31:30 +0100

    Initial Commit

---
+++ rdar-activityVC-slow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  rdar-activityVC-slow
+//
+//  Created by Manuel "StuFF mc" Carrasco Molina on 17/12/13.
+//  Copyright (c) 2013 Manuel "StuFF mc" Carrasco Molina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ rdar-activityVC-slow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  rdar-activityVC-slow
+//
+//  Created by Manuel "StuFF mc" Carrasco Molina on 17/12/13.
+//  Copyright (c) 2013 Manuel "StuFF mc" Carrasco Molina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
