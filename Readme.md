3fe45501f8481b7fff67b789fdc47b122d966a74
StuFF mc <mc@stuffmc.com>
2013-12-17 18:44:21 +0100

    First presentation of UIActivityViewController is slow

---
+++ Readme.md
@@ -0,0 +1,4 @@
+First presentation of UIActivityViewController is slow
+---
+- Press on `present` the first time: super slow :(
+- Press again: all fine.
--- rdar-activityVC-slow/ViewController.m
+++ rdar-activityVC-slow/ViewController.m
@@ -12,18 +12,24 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    UIActivityViewController *activityVC;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[@"foo"] applicationActivities:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)present:(id)sender {
+    [self presentViewController:activityVC animated:YES completion:nil];
 }
 
 @end
